8794d72bcb3f6cc882535bad81ef44cad7a78ff7
DevShishodia <dshishodia56@gmai.com>
2022-05-22 06:59:11 -0700

    change theme

---
+++ src/component/DarkTheme.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./DarkTheme.css";
+
+const DarkTheme = () => {
+  return (
+    <div className="DarkTheme">
+      <h1>Dark Theme Componet</h1>
+    </div>
+  );
+};
+
+export default DarkTheme;
---
+++ src/component/LightTheme.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./LightTheme.css";
+
+const LightTheme = () => {
+  return (
+    <div className="LigthTheme">
+      <h1>Ligth Theme Component</h1>
+    </div>
+  );
+};
+
+export default LightTheme;
