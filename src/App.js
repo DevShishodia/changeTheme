1be13d8aa6cd93e2605ad4f88f7b8211f4da2882
DevShishodia <dshishodia56@gmai.com>
2022-05-22 13:01:27 -0700

    change theme

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import React, { useState } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  const [state, setState] = useState(true);
+
+  const handleClick = () => {
+    if (state) {
+      setState(false);
+    } else {
+      setState(true);
+    }
+  };
+
+  return (
+    <div className={state ? "LigthTheme" : "DarkTheme"}>
+      <button onClick={handleClick}>Change Background</button>
+    </div>
+  );
+};
 
 export default App;
